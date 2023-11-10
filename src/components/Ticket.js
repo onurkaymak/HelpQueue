0b18a1f136c82943cec8808bb5a2495a0166a0ee
onurkaymak <onurkaymak@outlook.com>
2023-11-10 13:57:10 -0800

    Add proptypes in Ticket component.

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Ticket = (props) => {
 
@@ -10,5 +11,11 @@ const Ticket = (props) => {
     </React.Fragment>
   );
 }
+
+Ticket.propTypes = {
+  names: PropTypes.string,
+  location: PropTypes.string,
+  issue: PropTypes.string
+};
 
 export default Ticket;
