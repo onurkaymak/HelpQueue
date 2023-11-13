78ce210ca4b77d6a95451fe4e1194e1570748f18
onurkaymak <onurkaymak@outlook.com>
2023-11-12 20:56:19 -0800

    Refactor App.js for state changes.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from './Header';
-import TicketList from './TicketList';
+import TicketControl from "./TicketControl";
+
 
 const App = () => {
 
   return (
     <React.Fragment>
       <Header />
-      <TicketList />
+      <TicketControl />
     </React.Fragment>
   );
 }
