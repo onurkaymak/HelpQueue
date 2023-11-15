1d6960b9c9c10d3d8f75ff560fa05001c6d7220a
onurkaymak <onurkaymak@outlook.com>
2023-11-15 10:09:10 -0800

    Create EditTicketForm component.

---
+++ src/components/EditTicketForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReusableForm from "./ReusableForm";
+
+const EditTicketForm = () => {
+  return (
+    <React.Fragment>
+      <ReusableForm
+        buttonText="Update Ticket" />
+    </React.Fragment>
+  );
+};
+
+export default EditTicketForm;
+
