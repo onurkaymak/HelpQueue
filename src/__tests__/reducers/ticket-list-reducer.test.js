a94adab01a3463dad5d262ce922da395b9cb0ca2
onurkaymak <onurkaymak@outlook.com>
2023-11-24 14:29:41 -0800

    Pass test #2.

--- src/__tests__/reducers/ticket-list-reducer.test.js
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -1,7 +1,42 @@
 import ticketListReducer from '../../reducers/ticket-list-reducer';
 
 describe('ticketListReducer', () => {
+
+  let action;
+
+  const ticketData = {
+    names: 'Ryan & Aimen',
+    location: '4b',
+    issue: 'Redux action is not working correctly.',
+    id: 1
+  };
+
+
   test('#1 Should return default state if there is no action type passed into the reducer', () => {
     expect(ticketListReducer({}, { type: null })).toEqual({});
   });
+
+  test('#2 Should successfully add new ticket data to mainTicketList', () => {
+    const { names, location, issue, id } = ticketData;
+    action = {
+      type: 'ADD_TICKET',
+      names: names,
+      location: location,
+      issue: issue,
+      id: id
+    };
+
+    expect(ticketListReducer({}, action)).toEqual({
+      [id]: {
+        names: names,
+        location: location,
+        issue: issue,
+        id: id
+      }
+    });
+  });
+
+
+
+
 });
