8dd3a93194fc3f943c39138289ff81a73a49c149
onurkaymak <onurkaymak@outlook.com>
2023-11-24 13:39:41 -0800

    Refactor test #1 description.

--- src/__tests__/reducers/ticket-list-reducer.test.js
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -1,7 +1,7 @@
 import ticketListReducer from '../../reducers/ticket-list-reducer';
 
 describe('ticketListReducer', () => {
-  test('Should return default state if there is no action type passed into the reducer', () => {
+  test('#1 Should return default state if there is no action type passed into the reducer', () => {
     expect(ticketListReducer({}, { type: null })).toEqual({});
   });
 });
