6b5623d0ff3c68a94efb6991fb7da91757f403b9
onurkaymak <onurkaymak@outlook.com>
2023-11-24 20:24:14 -0800

    Refactor handleEditingTicketInList with Redux.

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -65,13 +65,20 @@ class TicketControl extends React.Component {
   }
 
   handleEditingTicketInList = (ticketToEdit) => {
-    const editedMainTicketList =
-      this.state.mainTicketList
-        .filter(ticket => ticket.id !== this.state.selectedTicket.id)
-        .concat(ticketToEdit);
+    const { dispatch } = this.props;
+    const { id, names, location, issue } = ticketToEdit;
+
+    const action = {
+      type: 'ADD_TICKET',
+      id: id,
+      names: names,
+      location: location,
+      issue: issue,
+    }
+
+    dispatch(action);
 
     this.setState({
-      mainTicketList: editedMainTicketList,
       editing: false,
       selectedTicket: null
     });
