b920e0d21d397ad9d2a22b95454e3016e2ed5be6
onurkaymak <onurkaymak@outlook.com>
2023-11-27 06:49:21 -0800

    Pass Test #5 for index reducer.

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -34,4 +34,12 @@ describe("rootReducer", () => {
     expect(store.getState().mainTicketList).toEqual(ticketListReducer(undefined, action));
   });
 
+  test('#5 Check that TOGGLE_FORM action works for formVisibleReducer and root reducer', () => {
+    const action = {
+      type: 'TOGGLE_FORM'
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
+  });
+
 });
