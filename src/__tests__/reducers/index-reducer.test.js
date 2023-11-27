12bc580023d6a9321534dc3ce2786189c0987c75
onurkaymak <onurkaymak@outlook.com>
2023-11-27 06:45:51 -0800

    Create root reducer, pass Smoke tests (#1, #2, #3).

---
+++ src/__tests__/reducers/index-reducer.test.js
@@ -0,0 +1,25 @@
+import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+import ticketListReducer from '../../reducers/ticket-list-reducer';
+
+let store = createStore(rootReducer);
+
+describe("rootReducer", () => {
+
+  test('#1 Should return default state if no action type is recognized', () => {
+    expect(rootReducer({}, { type: null })).toEqual({
+      mainTicketList: {},
+      formVisibleOnPage: false
+    });
+  });
+
+  test('#2 Check that initial state of ticketListReducer matches root reducer', () => {
+    expect(store.getState().mainTicketList).toEqual(ticketListReducer(undefined, { type: null }));
+  });
+
+  test('#3 Check that initial state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
+  });
+
+});
