30a47501ee866aae1594e4f123f6fe6102924d0c
onurkaymak <onurkaymak@outlook.com>
2023-11-27 06:27:22 -0800

    Pass Test #2 for form-visible reducer.

--- src/__tests__/reducers/form-visible-reducer.test.js
+++ src/__tests__/reducers/form-visible-reducer.test.js
@@ -2,7 +2,11 @@ import formVisibleReducer from '../../reducers/form-visible-reducer';
 
 describe("formVisibleReducer", () => {
 
-  test('Should return default state if no action type is recognized', () => {
+  test('#1 Should return default state if no action type is recognized', () => {
     expect(formVisibleReducer(false, { type: null })).toEqual(false);
+  });
+
+  test('#2 Should toggle form visibility state to true', () => {
+    expect(formVisibleReducer(false, { type: 'TOGGLE_FORM' })).toEqual(true);
   });
 });
