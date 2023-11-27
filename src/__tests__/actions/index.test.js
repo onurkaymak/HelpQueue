6a046f4d0ebe9914cb9a39fa443c91ae627a4b69
onurkaymak <onurkaymak@outlook.com>
2023-11-27 15:17:40 -0800

    Pass Test #2 for action creators.

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -7,4 +7,12 @@ describe('Help Queue actions', () => {
       id: 1
     });
   });
+
+  it('#2 toggleForm should create TOGGLE_FORM action', () => {
+    expect(actions.toggleForm()).toEqual({
+      type: 'TOGGLE_FORM'
+    });
+  });
+
+
 });
