8d4afce1a33cb45c5bee6d85965e4bc37348eda2
onurkaymak <onurkaymak@outlook.com>
2023-11-28 07:08:17 -0800

    Refactor and Pass Test #3 for action creators.

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -20,12 +20,16 @@ describe('Help Queue actions', () => {
       names: 'Jo and Jasmine',
       location: '3E',
       issue: 'Redux not working!',
+      timeOpen: 0,
+      formattedWaitTime: 'less than a minute ago',
       id: 1
     })).toEqual({
-      type: 'ADD_TICKET',
+      type: c.ADD_TICKET,
       names: 'Jo and Jasmine',
       location: '3E',
       issue: 'Redux not working!',
+      timeOpen: 0,
+      formattedWaitTime: 'less than a minute ago',
       id: 1
     });
   });
