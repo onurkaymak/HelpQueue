283f96752729ec03b1551a4020b3ead8d1fc7d34
onurkaymak <onurkaymak@outlook.com>
2023-11-29 06:55:38 -0800

    Refactor NewTicketForm component for date-fns.

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { v4 } from 'uuid';
 import ReusableForm from "./ReusableForm";
+import { formatDistanceToNow } from 'date-fns';
 
 const NewTicketForm = (props) => {
 
@@ -11,7 +12,11 @@ const NewTicketForm = (props) => {
       names: event.target.names.value,
       location: event.target.location.value,
       issue: event.target.issue.value,
-      id: v4()
+      id: v4(),
+      timeOpen: new Date(),
+      formattedWaitTime: formatDistanceToNow(new Date(), {
+        addSuffix: true
+      })
     });
   }
 
