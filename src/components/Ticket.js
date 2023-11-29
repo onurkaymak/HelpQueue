09c69985705d1660e9a5d8fa4c679e59e1b19f88
onurkaymak <onurkaymak@outlook.com>
2023-11-29 07:00:16 -0800

    Add formattedWaitTime as a prop type to Ticket component, add new propType.

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -8,6 +8,7 @@ const Ticket = (props) => {
       <div onClick={() => props.whenTicketClicked(props.id)}>
         <h3>{props.location} - {props.names}</h3>
         <p><em>{props.issue}</em></p>
+        <p><em>{props.formattedWaitTime}</em></p>
         <hr />
       </div>
     </React.Fragment>
@@ -17,7 +18,8 @@ const Ticket = (props) => {
 Ticket.propTypes = {
   names: PropTypes.string,
   location: PropTypes.string,
-  issue: PropTypes.string
+  issue: PropTypes.string,
+  formattedWaitTime: PropTypes.string
 };
 
 export default Ticket;
