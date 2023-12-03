c0ab7e51fa39aa03a014ab5dd00df139933db150
onurkaymak <onurkaymak@outlook.com>
2023-12-02 21:06:03 -0800

    Refactor formVisibleOnPage state.

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -7,15 +7,15 @@ import TicketDetail from './TicketDetail';
 const TicketControl = () => {
   const [formVisibleOnPage, setFormVisibleOnPage] = useState(false);
   const [mainTicketList, setMainTicketList] = useState([]);
+  const [selectedTicket, setSelectedTicket] = useState(null);
+  const [editing, setEditing] = useState(false);
 
 
   const handleClick = () => {
-    if (this.state.selectedTicket != null) {
+    if (selectedTicket != null) {
       setFormVisibleOnPage(false);
-      this.setState({
-        formVisibleOnPage: false,
-        selectedTicket: null,
-      });
+      setSelectedTicket(null);
+      setEditing(false);
     } else {
       setFormVisibleOnPage(!formVisibleOnPage);
     }
@@ -24,17 +24,20 @@ const TicketControl = () => {
   const handleDeletingTicket = (id) => {
     const newMainTicketList = mainTicketList.filter(ticket => ticket.id !== id);
     setMainTicketList(newMainTicketList);
+    setSelectedTicket(null);
   }
 
   const handleEditClick = () => {
-    this.setState({ editing: true });
+    setEditing(true);
   }
 
   const handleEditingTicketInList = (ticketToEdit) => {
     const editedMainTicketList = mainTicketList
-      .filter(ticket => ticket.id !== this.state.selectedTicket.id)
+      .filter(ticket => ticket.id !== selectedTicket.id)
       .concat(ticketToEdit);
     setMainTicketList(editedMainTicketList);
+    setEditing(false);
+    setSelectedTicket(null);
   }
 
   const handleAddingNewTicketToList = (newTicket) => {
@@ -44,22 +47,28 @@ const TicketControl = () => {
   }
 
   const handleChangingSelectedTicket = (id) => {
-    const selectedTicket = this.state.mainTicketList.filter(ticket => ticket.id === id)[0];
-    this.setState({ selectedTicket: selectedTicket });
+    // new code: updated variable name to 'selection'
+    // so there's no clash with the state variable 'selectedTicket'
+    const selection = mainTicketList.filter(ticket => ticket.id === id)[0];
+    setSelectedTicket(selection);
   }
 
 
   let currentlyVisibleState = null;
   let buttonText = null;
 
-  if (this.state.editing) {
-    currentlyVisibleState = <EditTicketForm ticket={this.state.selectedTicket} onEditTicket={this.handleEditingTicketInList} />
+  if (editing) {
+    currentlyVisibleState =
+      <EditTicketForm
+        ticket={selectedTicket}
+        onEditTicket={this.handleEditingTicketInList} />
     buttonText = "Return to Ticket List";
-  } else if (this.state.selectedTicket != null) {
-    currentlyVisibleState = <TicketDetail
-      ticket={this.state.selectedTicket}
-      onClickingDelete={this.handleDeletingTicket}
-      onClickingEdit={this.handleEditClick} />
+  } else if (selectedTicket != null) {
+    currentlyVisibleState =
+      <TicketDetail
+        ticket={selectedTicket}
+        onClickingDelete={this.handleDeletingTicket}
+        onClickingEdit={this.handleEditClick} />
     buttonText = "Return to Ticket List";
   } else if (formVisibleOnPage) {
     currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList} />;
