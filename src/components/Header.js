2f2b3931a53c9ac7c9b66cde02b3148da01778ea
onurkaymak <onurkaymak@outlook.com>
2023-12-04 06:20:35 -0800

    Refactor Header, add router links in Header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,18 @@
 import React from "react";
-import ticketsImage from "./../img/tickets.png";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <React.Fragment>
-      <h1>Help Queue</h1>
-      <img src={ticketsImage} alt="tickets" />
+      <h1> Help Queue</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/sign-in">Sign In</Link>
+        </li>
+      </ul>
     </React.Fragment>
   );
 }
