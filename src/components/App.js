ce1af4cc54a162d605881155b2673588501a5db6
onurkaymak <onurkaymak@outlook.com>
2023-12-04 06:18:18 -0800

    Add a router in APP.

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import Header from './Header';
 import TicketControl from "./TicketControl";
 
+import SignIn from "./SignIn";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 const App = () => {
-
   return (
-    <React.Fragment>
+    <Router>
       <Header />
-      <TicketControl />
-    </React.Fragment>
+      <Routes>
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/" element={<TicketControl />} />
+      </Routes>
+    </Router>
   );
 }
 
