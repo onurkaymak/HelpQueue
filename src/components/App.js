9d023114237c2946bc380e07bf8373ab38058992
onurkaymak <onurkaymak@outlook.com>
2023-12-10 19:37:50 -0800

    Add Provider in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { useState } from "react";
 import Header from './Header';
 import TicketControl from "./TicketControl";
 import ToggleTheme from "./ToggleTheme";
+import { ThemeContext, themes } from "../context/theme-context";
 
 import SignIn from "./SignIn";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
 const App = () => {
+
+  const [theme, setTheme] = useState(themes.light);
+
   return (
     <Router>
-      <Header />
-      <ToggleTheme />
-      <Routes>
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/" element={<TicketControl />} />
-      </Routes>
+      <ThemeContext.Provider value={theme}>
+        <Header />
+        <ToggleTheme />
+        <Routes>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/" element={<TicketControl />} />
+        </Routes>
+      </ThemeContext.Provider>
     </Router>
   );
 }
