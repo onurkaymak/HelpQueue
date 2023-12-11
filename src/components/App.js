c231d3d0b4b800f8cc2190861a7f7fa832bc39d6
onurkaymak <onurkaymak@outlook.com>
2023-12-10 19:23:08 -0800

    Add ToggleTheme in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import TicketControl from "./TicketControl";
+import ToggleTheme from "./ToggleTheme";
 
 import SignIn from "./SignIn";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -10,6 +11,7 @@ const App = () => {
   return (
     <Router>
       <Header />
+      <ToggleTheme />
       <Routes>
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/" element={<TicketControl />} />
